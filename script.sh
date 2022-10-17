50f252e17fe1b270c390baceb7a1a56a1bc928c3
PraveenReddy I <narayana.praveen143@gmail.com>
2022-10-17 17:22:42 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,22 +1,5 @@
 #! /bin/sh
 
-sudo apt-get update -y
-sudo apt-get install \
-    ca-certificates \
-    curl \
-    gnupg \
-    lsb-release
-sudo mkdir -p /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update -y
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
-sudo chmod 666 /var/run/docker.sock
-sudo newgroup docker
-sudo chmod 666 /var/run/docker.sock
-sudo usermod -aG docker ${USER}
-sudo usermod -aG docker ${ubuntu}
-sudo docker run hello-world
-docker images
+pwd
+ls -lrt
+sudo mkdir -p /home/ubuntu/praveen
